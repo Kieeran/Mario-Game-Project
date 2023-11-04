c6ccf2fb90896baeeb109967eb90f6fcedc4751d
Nguyễn Phạm Quốc Bảo <21521856@ms.uit.edu.vn>
2023-11-04 11:14:13 +0700

    Build a new ground

---
+++ Ground.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "GameObject.h"
+#include "Animation.h"
+#include "Animations.h"
+
+#define ID_ANI_GROUND 300000
+#define GROUND_WIDTH 16
+#define GROUND_BBOX_WIDTH 16
+#define GROUND_BBOX_HEIGHT 16
+
+class CGround : public CGameObject
+{
+public:
+	CGround(float x, float y) : CGameObject(x, y) {}
+	void Render();
+	void Update(DWORD dt) {}
+	void GetBoundingBox(float& l, float& t, float& r, float& b);
+};
