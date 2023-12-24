932c25f3166dbadddbeef56f7099da503d4d2e45
Nguyễn Phạm Quốc Bảo <21521856@ms.uit.edu.vn>
2023-12-24 23:52:12 +0700

    Reuse the code from the Ground object for the HardBlock object. Add scenes.

---
+++ HardBlock.cpp
@@ -0,0 +1,16 @@
+#include "HardBlock.h"
+
+void CHardBlock::Render()
+{
+	CAnimations* animations = CAnimations::GetInstance();
+	animations->Get(ID_ANI_TOP_GROUND)->Render(x, y);
+	//RenderBoundingBox();
+}
+
+void CHardBlock::GetBoundingBox(float& l, float& t, float& r, float& b)
+{
+	l = x - HARDBLOCK_BBOX_WIDTH / 2;
+	t = y - HARDBLOCK_BBOX_HEIGHT / 2;
+	r = l + HARDBLOCK_BBOX_WIDTH;
+	b = t + HARDBLOCK_BBOX_HEIGHT;
+}
---
+++ HardBlock.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "GameObject.h"
+#include "Animation.h"
+#include "Animations.h"
+#include "AssetIDs.h"
+
+#define HARDBLOCK_WIDTH 16
+#define HARDBLOCK_BBOX_WIDTH 16
+#define HARDBLOCK_BBOX_HEIGHT 16
+
+class CHardBlock : public CGameObject
+{
+public:
+	CHardBlock(float x, float y) : CGameObject(x, y) {}
+	void Render();
+	void Update(DWORD dt) {}
+	void GetBoundingBox(float& l, float& t, float& r, float& b);
+};
