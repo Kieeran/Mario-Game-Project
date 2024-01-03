6178f59bddf67d8101384da668c08cfd11437081
Nguyễn Phạm Quốc Bảo <21521856@ms.uit.edu.vn>
2024-01-03 21:20:32 +0700

    Complete the movement part of the Piranha Plant

--- PiranhaPlant.cpp
+++ PiranhaPlant.cpp
@@ -7,12 +7,129 @@ CPiranhaPlant::CPiranhaPlant(float x, float y, int plantType) : CGameObject(x, y
 	this->plantType = plantType;
 	SetState(PIRANHA_PLANT_STATE_SLEEP);
 	Origin_Y = y;
-	rising = false;
-	sleeping = false;
-	Up = false;
-	Left = false;
-	rising_start = 0;
 	reload = -1;
+	waiting = -1;
+	sleepTime = -1;
+}
+
+int CPiranhaPlant::GetAniIdRedFirePiranhaPlant()
+{
+	CMario* mario = (CMario*)((LPPLAYSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
+
+	int aniId = -1;
+	if (x > mario->GetX() && y <= mario->GetY())			// mario in the bottom left corner
+	{
+		if (vy != 0)
+		{
+			aniId = ID_ANI_RED_FIRE_PIRANHA_PLANT_LOOKDOWN_LEFT;
+		}
+
+		else if (Origin_Y - y >= RED_FIRE_PIRANHA_PLANT_RISEUP_HEIGHT_MAX)
+		{
+			aniId = ID_ANI_RED_FIRE_PIRANHA_PLANT_IDLE_LOOKDOWN_LEFT;
+		}
+	}
+
+	else if (x > mario->GetX() && y > mario->GetY())	// mario in the upper left corner
+	{
+		if (vy != 0)
+		{
+			aniId = ID_ANI_RED_FIRE_PIRANHA_PLANT_LOOKUP_LEFT;
+		}
+
+		else if (Origin_Y - y >= RED_FIRE_PIRANHA_PLANT_RISEUP_HEIGHT_MAX)
+		{
+			aniId = ID_ANI_RED_FIRE_PIRANHA_PLANT_IDLE_LOOKUP_LEFT;
+		}
+	}
+
+	else if (x <= mario->GetX() && y <= mario->GetY())	// mario in the bottom right corner
+	{
+		if (vy != 0)
+		{
+			aniId = ID_ANI_RED_FIRE_PIRANHA_PLANT_LOOKDOWN_RIGHT;
+		}
+
+		else if (Origin_Y - y >= RED_FIRE_PIRANHA_PLANT_RISEUP_HEIGHT_MAX)
+		{
+			aniId = ID_ANI_RED_FIRE_PIRANHA_PLANT_IDLE_LOOKDOWN_RIGHT;
+		}
+	}
+
+	else if (x <= mario->GetX() && y > mario->GetY())	// mario in the upper right corner
+	{
+		if (vy != 0)
+		{
+			aniId = ID_ANI_RED_FIRE_PIRANHA_PLANT_LOOKUP_RIGHT;
+		}
+
+		else if (Origin_Y - y >= RED_FIRE_PIRANHA_PLANT_RISEUP_HEIGHT_MAX)
+		{
+			aniId = ID_ANI_RED_FIRE_PIRANHA_PLANT_IDLE_LOOKUP_RIGHT;
+		}
+	}
+
+	return aniId;
+}
+int CPiranhaPlant::GetAniIdGreenFirePiranhaPlant()
+{
+	CMario* mario = (CMario*)((LPPLAYSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
+
+	int aniId = -1;
+
+	if (x > mario->GetX() && y <= mario->GetY())			// mario in the bottom left corner
+	{
+		if (vy != 0)
+		{
+			aniId = ID_ANI_GREEN_FIRE_PIRANHA_PLANT_LOOKDOWN_LEFT;
+		}
+
+		else if (Origin_Y - y >= GREEN_FIRE_PIRANHA_PLANT_RISEUP_HEIGHT_MAX)
+		{
+			aniId = ID_ANI_GREEN_FIRE_PIRANHA_PLANT_IDLE_LOOKDOWN_LEFT;
+		}
+	}
+
+	else if (x > mario->GetX() && y > mario->GetY())	// mario in the upper left corner
+	{
+		if (vy != 0)
+		{
+			aniId = ID_ANI_GREEN_FIRE_PIRANHA_PLANT_LOOKUP_LEFT;
+		}
+
+		else if (Origin_Y - y >= GREEN_FIRE_PIRANHA_PLANT_RISEUP_HEIGHT_MAX)
+		{
+			aniId = ID_ANI_GREEN_FIRE_PIRANHA_PLANT_IDLE_LOOKUP_LEFT;
+		}
+	}
+
+	else if (x <= mario->GetX() && y <= mario->GetY())	// mario in the bottom right corner
+	{
+		if (vy != 0)
+		{
+			aniId = ID_ANI_GREEN_FIRE_PIRANHA_PLANT_LOOKDOWN_RIGHT;
+		}
+
+		else if (Origin_Y - y >= GREEN_FIRE_PIRANHA_PLANT_RISEUP_HEIGHT_MAX)
+		{
+			aniId = ID_ANI_GREEN_FIRE_PIRANHA_PLANT_IDLE_LOOKDOWN_RIGHT;
+		}
+	}
+
+	else if (x <= mario->GetX() && y > mario->GetY())	// mario in the upper right corner
+	{
+		if (vy != 0)
+		{
+			aniId = ID_ANI_GREEN_FIRE_PIRANHA_PLANT_LOOKUP_RIGHT;
+		}
+
+		else if (Origin_Y - y >= GREEN_FIRE_PIRANHA_PLANT_RISEUP_HEIGHT_MAX)
+		{
+			aniId = ID_ANI_GREEN_FIRE_PIRANHA_PLANT_IDLE_LOOKUP_RIGHT;
+		}
+	}
+
+	return aniId;
 }
 
 void CPiranhaPlant::Render()
@@ -22,79 +139,24 @@ void CPiranhaPlant::Render()
 	CAnimations* animations = CAnimations::GetInstance();
 	int aniId = -1;
 
-	if (plantType == GREEN_PIRANHA_PLANT)
+	if (plantType == RED_FIRE_PIRANHA_PLANT)
+	{
+		aniId = GetAniIdRedFirePiranhaPlant();
+	}
+
+	else if (plantType == GREEN_FIRE_PIRANHA_PLANT)
+	{
+		aniId = GetAniIdGreenFirePiranhaPlant();
+	}
+
+	else if (plantType == GREEN_PIRANHA_PLANT)
 	{
 		aniId = ID_ANI_GREEN_PIRANHA_PLANT;
-		//animations->Get(ID_ANI_GREENFLOWER)->Render(x, y);
 	}
-
-	else
-	{
-
-		if (plantType == RED_FIRE_PIRANHA_PLANT)
-		{
-			aniId = ID_ANI_RED_FIRE_PIRANHA_PLANT_LOOKDOWN_LEFT;
-			//aniId = ID_ANI_RED_FIRE_PIRANHA_PLANT_LOOKUP_LEFT;
-			//aniId = ID_ANI_RED_FIRE_PIRANHA_PLANT_LOOKDOWN_RIGHT;
-			//aniId = ID_ANI_RED_FIRE_PIRANHA_PLANT_LOOKUP_RIGHT;
-
-			//aniId = ID_ANI_RED_FIRE_PIRANHA_PLANT_IDLE_LOOKDOWN_LEFT;
-			//aniId = ID_ANI_RED_FIRE_PIRANHA_PLANT_IDLE_LOOKUP_LEFT;
-			//aniId = ID_ANI_RED_FIRE_PIRANHA_PLANT_IDLE_LOOKDOWN_RIGHT;
-			//aniId = ID_ANI_RED_FIRE_PIRANHA_PLANT_IDLE_LOOKUP_RIGHT;
-		}
-
-		else if (plantType == GREEN_FIRE_PIRANHA_PLANT)
-		{
-			//aniId = ID_ANI_GREEN_FIRE_PIRANHA_PLANT_LOOKDOWN_LEFT;
-			//aniId = ID_ANI_GREEN_FIRE_PIRANHA_PLANT_LOOKUP_LEFT;
-			//aniId = ID_ANI_GREEN_FIRE_PIRANHA_PLANT_LOOKDOWN_RIGHT;
-			//aniId = ID_ANI_GREEN_FIRE_PIRANHA_PLANT_LOOKUP_RIGHT;
-
-			//aniId = ID_ANI_GREEN_FIRE_PIRANHA_PLANT_IDLE_LOOKDOWN_LEFT;
-			//aniId = ID_ANI_GREEN_FIRE_PIRANHA_PLANT_IDLE_LOOKUP_LEFT;
-			//aniId = ID_ANI_GREEN_FIRE_PIRANHA_PLANT_IDLE_LOOKDOWN_RIGHT;
-			aniId = ID_ANI_GREEN_FIRE_PIRANHA_PLANT_IDLE_LOOKUP_RIGHT;
-		}
-
-		//CMario* mario = (CMario*)((LPPLAYSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
-		//if (x > mario->GetX() && y < mario->GetY())		// Mario is at the bottom-left corner
-		//{
-		//	if (state != PIRANHA_PLANT_STATE_IDLE)
-		//	{
-
-		//	}
-
-		//	else
-		//	{
-
-		//	}
-		//}
-		//	case FIREFLOWER_STATE_LOOKDOWN_LEFT:				//fire flower rise up look down left
-		//		animations->Get(ID_ANI_FIREFLOWER_LOOKDOWN_LEFT)->Render(x, y);
-		//		return;
-		//	case FIREFLOWER_STATE_IDLE_LOOKDOWN_LEFT:			//fire flower look down left
-		//		s->Get(ID_SPRITE_FIREFLOWER_1)->Draw(x, y);
-		//		return;
-		//	case FIREFLOWER_STATE_LOOKUP_LEFT:					//fire flower rise up look up left
-		//		animations->Get(ID_ANI_FIREFLOWER_LOOKUP_LEFT)->Render(x, y);
-		//		return;
-		//	case FIREFLOWER_STATE_IDLE_LOOKUP_LEFT:				//fire flower look up left
-		//		s->Get(ID_SPRITE_FIREFLOWER_3)->Draw(x, y);
-
-		//	case FIREFLOWER_STATE_LOOKDOWN_RIGHT:				//fire flower rise up look down right
-		//		animations->Get(ID_ANI_FIREFLOWER_LOOKDOWN_RIGHT)->Render(x, y);
-		//	case FIREFLOWER_STATE_IDLE_LOOKDOWN_RIGHT:			//fire flower look down right
-		//		s->Get(ID_SPRITE_FIREFLOWER_8)->Draw(x, y);
-		//	case FIREFLOWER_STATE_LOOKUP_RIGHT:					//fire flower rise up look up right
-		//		animations->Get(ID_ANI_FIREFLOWER_LOOKUP_RIGHT)->Render(x, y);
-		//	case FIREFLOWER_STATE_IDLE_LOOKUP_RIGHT:			//fire flower look up right
-		//		s->Get(ID_SPRITE_FIREFLOWER_6)->Draw(x, y);
-	}
-	//animations->Get(aniId)->Render(x, y);
+	if (aniId == -1) return;
 	animations->Get(aniId)->Render(x, y);
 
-	//RenderBoundingBox();
+	RenderBoundingBox();
 }
 
 void CPiranhaPlant::OnNoCollision(DWORD dt)
@@ -104,132 +166,87 @@ void CPiranhaPlant::OnNoCollision(DWORD dt)
 
 void CPiranhaPlant::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
-	/*CPlayScene* scene = (CPlayScene*)CGame::GetInstance()->GetCurrentScene();
-	CMario* mario = (CMario*)((LPPLAYSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
 
-	if (flowerType == RED_FIREFLOWER_TYPE)
+	float heightMax = -1;
+	if (plantType == RED_FIRE_PIRANHA_PLANT)
 	{
-		if (this->x > mario->GetX() && this->y < mario->GetY())
-		{
-			if (rising || sleeping)
-				SetState(FIREFLOWER_STATE_LOOKDOWN_LEFT);
-			else
-				SetState(FIREFLOWER_STATE_IDLE_LOOKDOWN_LEFT);
-		}
-		else if (this->x > mario->GetX() && this->y > mario->GetY())
-		{
-			if (rising || sleeping)
-				SetState(FIREFLOWER_STATE_LOOKUP_LEFT);
-			else
-				SetState(FIREFLOWER_STATE_IDLE_LOOKUP_LEFT);
-		}
-		else if (this->x < mario->GetX() && this->y < mario->GetY())
-		{
-			if (rising || sleeping)
-				SetState(FIREFLOWER_STATE_LOOKDOWN_RIGHT);
-			else
-				SetState(FIREFLOWER_STATE_IDLE_LOOKDOWN_RIGHT);
-		}
-		else if (this->x < mario->GetX() && this->y > mario->GetY())
-		{
-			if (rising || sleeping)
-				SetState(FIREFLOWER_STATE_LOOKUP_RIGHT);
-			else
-				SetState(FIREFLOWER_STATE_IDLE_LOOKUP_RIGHT);
-		}
+		heightMax = RED_FIRE_PIRANHA_PLANT_RISEUP_HEIGHT_MAX;
+	}
 
-		if (GetTickCount64() - reload > TIME_BULLET_DELETE && reload != 0)
-		{
-			reload = 0;
-		}
-
-		if (IsMarioOnTriggerDistance() && rising_start == 0)
-		{
-			rising_start = GetTickCount64();
-			rising = true;
-		}
-
-		if (GetTickCount64() - rising_start > FIREFLOWER_RISING_TIME && rising_start != 0)
-		{
-			if (IsMarioOnTriggerDistance() && !sleeping)
-			{
-				if (reload == 0)
-				{
-					CFireBullet* bullet = new CFireBullet(x, y, Up, Left);
-					scene->AddObject(bullet, ADD_OBJECT_MODE_1);
-					reload = GetTickCount64();
-					waiting = GetTickCount64();
-				}
-			}
-			else
-				sleeping = true;
-		}
-
-		if (GetTickCount64() - waiting > FIREFLOWER_WAITING_TIME && waiting != 0)
-		{
-			waiting = 0;
-			sleeping = true;
-		}
-
-		if (rising)
-		{
-			y -= vy * dt;
-			if (this->Origin_Y - this->y >= FLOWER_RISEUP_HEIGHT_MAX)
-			{
-				this->y = this->Origin_Y - FLOWER_RISEUP_HEIGHT_MAX;
-				rising = false;
-			}
-		}
-		else if (sleeping)
-		{
-			y += vy * dt;
-			if (this->y >= this->Origin_Y)
-			{
-				this->y = this->Origin_Y;
-				sleeping = false;
-				SetState(FLOWER_STATE_SLEEP);
-				rising_start = 0;
-			}
-		}
+	else if (plantType == GREEN_FIRE_PIRANHA_PLANT)
+	{
+		heightMax = GREEN_FIRE_PIRANHA_PLANT_RISEUP_HEIGHT_MAX;
 	}
 
 	else
 	{
-		if (IsMarioOnTriggerDistance() && state == FLOWER_STATE_SLEEP && rising_start <= 0)
-		{
-			SetState(GREENFLOWER_STATE_RISING);
-			vy = -FLOWER_RISEUP_SPEED;
-			DebugOut(L">>> start rising >>> \n");
-		}
+		heightMax = GREEN_PIRANHA_PLANT_RISEUP_HEIGHT_MAX;
+	}
 
-		if (Origin_Y - y > FLOWER_RISEUP_HEIGHT_MAX)
-		{
-			y = Origin_Y - FLOWER_RISEUP_HEIGHT_MAX;
-			vy = 0;
-			rising_start = GetTickCount64();
-			DebugOut(L">>> start waiting >>> \n");
-		}
+	if (state == PIRANHA_PLANT_STATE_SLEEP && isMarioOnTriggerDistance())
+	{
+		SetState(PIRANHA_PLANT_STATE_RISING);
+		vy = -PIRANHA_PLANT_RISEUP_SPEED;
+		//DebugOut(L">>> rising >>> \n");
+	}
 
-		if (GetTickCount64() - rising_start > 500 && rising_start > 0)
-		{
-			vy = FLOWER_RISEUP_SPEED;
-			DebugOut(L">>> start !rising >>> \n");
-		}
+	if (Origin_Y - y > heightMax)
+	{
+		y = Origin_Y - heightMax;
+		vy = 0;
+		SetState(PIRANHA_PLANT_STATE_IDLE);
+		//DebugOut(L">>> waiting >>> \n");
+		waiting = GetTickCount64();
+	}
 
-		if (GetTickCount64() - rising_start > 3000 && rising_start > 0)
-		{
-			rising_start = 0;
-			DebugOut(L">>> done >>> \n");
-		}
+	if (GetTickCount64() - waiting > PIRANHA_PLANT_WAITING_TIME && waiting > 0)
+	{
+		waiting = 0;
 
-		if (Origin_Y - y < 0)
+		if (plantType == GREEN_PIRANHA_PLANT)
 		{
-			y = Origin_Y;
-			vy = 0;
-			SetState(FLOWER_STATE_SLEEP);
-			DebugOut(L">>> sleep >>> \n");
+			//DebugOut(L">>> diving >>> \n");
+			vy = PIRANHA_PLANT_RISEUP_SPEED;
+			SetState(PIRANHA_PLANT_STATE_DIVING);
 		}
-	}*/
+		else
+		{
+			if (isMarioOnTriggerDistance())
+			{
+				//DebugOut(L">>> reloading >>> \n");
+				reload = GetTickCount64();
+			}
+			else
+			{
+				//DebugOut(L">>> diving >>> \n");
+				vy = PIRANHA_PLANT_RISEUP_SPEED;
+				SetState(PIRANHA_PLANT_STATE_DIVING);
+			}
+		}
+	}
+
+	if (GetTickCount64() - reload > PIRANHA_PLANT_RELOADING_TIME && reload > 0)
+	{
+		reload = 0;
+		//DebugOut(L">>> diving >>> \n");
+		vy = PIRANHA_PLANT_RISEUP_SPEED;
+		SetState(PIRANHA_PLANT_STATE_DIVING);
+	}
+
+	if (y > Origin_Y)
+	{
+		y = Origin_Y;
+		vy = 0;
+		sleepTime = GetTickCount64();
+		//DebugOut(L">>> waiting >>> \n");
+	}
+
+	if (GetTickCount64() - sleepTime > PIRANHA_PLANT_WAITING_TIME && sleepTime > 0)
+	{
+		sleepTime = 0;
+		SetState(PIRANHA_PLANT_STATE_SLEEP);
+		//DebugOut(L">>> sleeping >>> \n");
+	}
 
 	CGameObject::Update(dt, coObjects);
 	CCollision::GetInstance()->Process(this, dt, coObjects);
@@ -237,10 +254,10 @@ void CPiranhaPlant::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 
 void CPiranhaPlant::GetBoundingBox(float& l, float& t, float& r, float& b)
 {
-	/*l = x - PLANT_BBOX_WIDTH / 2;
-	t = y - PLANT_BBOX_HEIGHT / 2;
-	r = l + PLANT_BBOX_WIDTH;
-	b = t + PLANT_BBOX_HEIGHT;*/
+	l = x - PIRANHA_PLANT_BBOX_WIDTH / 2;
+	t = y - PIRANHA_PLANT_BBOX_HEIGHT / 2;
+	r = l + PIRANHA_PLANT_BBOX_WIDTH;
+	b = t + PIRANHA_PLANT_BBOX_HEIGHT;
 }
 void CPiranhaPlant::SetState(int state)
 {
--- PiranhaPlant.h
+++ PiranhaPlant.h
@@ -14,17 +14,22 @@
 
 #define PIRANHA_PLANT_RISEUP_SPEED		0.03f
 
-#define RED_PIRANHA_PLANT_RISEUP_HEIGHT_MAX		35.0f
-#define GREEN_PIRANHA_PLANT_RISEUP_HEIGHT_MAX	24.0f
+#define RED_FIRE_PIRANHA_PLANT_RISEUP_HEIGHT_MAX		40.0f
+#define GREEN_FIRE_PIRANHA_PLANT_RISEUP_HEIGHT_MAX		32.0f
+#define GREEN_PIRANHA_PLANT_RISEUP_HEIGHT_MAX			24.0f
 
 #define PIRANHA_PLANT_TRIGGER_DISTANCE_OUTER_LIMIT	100.0f
 #define PIRANHA_PLANT_TRIGGER_DISTANCE_INNER_LIMIT	30.0f
 
-#define PIRANHA_PLANT_RISING_TIME		2000
-#define PIRANHA_PLANT_WAITING_TIME		1000
+#define PIRANHA_PLANT_WAITING_TIME		800
+#define PIRANHA_PLANT_RELOADING_TIME	1000
 
 #define PIRANHA_PLANT_BBOX_WIDTH	16
 #define PIRANHA_PLANT_BBOX_HEIGHT	32
+
+#define RED_FIRE_PIRANHA_PLANT_HEIGHT	35
+#define GREEN_FIRE_PIRANHA_PLANT_HEIGHT	24
+#define GREEN_PIRANHA_PLANT_HEIGHT		24
 
 #define PIRANHA_PLANT_STATE_SLEEP	100
 #define PIRANHA_PLANT_STATE_RISING	200
@@ -37,19 +42,18 @@ protected:
 	float Origin_Y;
 	int plantType;
 
-	bool rising;
-	bool sleeping;
-	bool Up;
-	bool Left;
 	ULONGLONG reload;
-	ULONGLONG rising_start;
 	ULONGLONG waiting;
+	ULONGLONG sleepTime;
 
 	void Render();
 	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
 
 	void OnNoCollision(DWORD dt);
+
+	int GetAniIdRedFirePiranhaPlant();
+	int GetAniIdGreenFirePiranhaPlant();
 
 	bool isMarioOnTriggerDistance()
 	{
