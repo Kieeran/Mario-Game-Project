834d2f6b6743e2cede272d09ad35e94bf18c743d
Nguyễn Phạm Quốc Bảo <21521856@ms.uit.edu.vn>
2024-01-09 16:02:43 +0700

    Add numbers to the HUD.

--- Hud.cpp
+++ Hud.cpp
@@ -4,11 +4,45 @@ void CHud::Render()
 {
 	CAnimations* animations = CAnimations::GetInstance();
 	animations->Get(ID_ANI_HUD)->Render(x, y);
+	DrawNumber(1, x, y);
+	DrawNumber(2, x + 10.0f, y);
 }
-void CHud::GetBoundingBox(float& l, float& t, float& r, float& b)
+
+void CHud::DrawNumber(int num, float x, float y)
 {
-	l = 0;
-	t = 0;
-	r = 1;
-	b = 1;
+	int aniId = -1;
+	switch (num)
+	{
+	case 0:
+		aniId = ID_ANI_NUM_0;
+		break;
+	case 1:
+		aniId = ID_ANI_NUM_1;
+		break;
+	case 2:
+		aniId = ID_ANI_NUM_2;
+		break;
+	case 3:
+		aniId = ID_ANI_NUM_3;
+		break;
+	case 4:
+		aniId = ID_ANI_NUM_4;
+		break;
+	case 5:
+		aniId = ID_ANI_NUM_5;
+		break;
+	case 6:
+		aniId = ID_ANI_NUM_6;
+		break;
+	case 7:
+		aniId = ID_ANI_NUM_7;
+		break;
+	case 8:
+		aniId = ID_ANI_NUM_8;
+		break;
+	case 9:
+		aniId = ID_ANI_NUM_9;
+		break;
+	}
+	CAnimations::GetInstance()->Get(aniId)->Render(x, y);
 }
--- Hud.h
+++ Hud.h
@@ -5,11 +5,38 @@
 #include "Animations.h"
 #include "AssetIDs.h"
 
+#define ID_SPRITE_HUD	100100
+#define ID_ANI_HUD		100150
+
+#define ID_SPRITE_NUM_0		100200
+#define ID_SPRITE_NUM_1		(ID_SPRITE_NUM_0 + 1)
+#define ID_SPRITE_NUM_2		(ID_SPRITE_NUM_0 + 2)
+#define ID_SPRITE_NUM_3		(ID_SPRITE_NUM_0 + 3)
+#define ID_SPRITE_NUM_4		(ID_SPRITE_NUM_0 + 4)
+#define ID_SPRITE_NUM_5		(ID_SPRITE_NUM_0 + 5)
+#define ID_SPRITE_NUM_6		(ID_SPRITE_NUM_0 + 6)
+#define ID_SPRITE_NUM_7		(ID_SPRITE_NUM_0 + 7)
+#define ID_SPRITE_NUM_8		(ID_SPRITE_NUM_0 + 8)
+#define ID_SPRITE_NUM_9		(ID_SPRITE_NUM_0 + 9)
+
+#define ID_ANI_NUM_0		100250
+#define ID_ANI_NUM_1		(ID_ANI_NUM_0 + 1)
+#define ID_ANI_NUM_2		(ID_ANI_NUM_0 + 2)
+#define ID_ANI_NUM_3		(ID_ANI_NUM_0 + 3)
+#define ID_ANI_NUM_4		(ID_ANI_NUM_0 + 4)
+#define ID_ANI_NUM_5		(ID_ANI_NUM_0 + 5)
+#define ID_ANI_NUM_6		(ID_ANI_NUM_0 + 6)
+#define ID_ANI_NUM_7		(ID_ANI_NUM_0 + 7)
+#define ID_ANI_NUM_8		(ID_ANI_NUM_0 + 8)
+#define ID_ANI_NUM_9		(ID_ANI_NUM_0 + 9)
+
 class CHud : public CGameObject
 {
 public:
 	CHud(float x, float y) : CGameObject(x, y) {}
 	void Render();
 	void Update(DWORD dt) {}
-	void GetBoundingBox(float& l, float& t, float& r, float& b);
+	void GetBoundingBox(float& l, float& t, float& r, float& b) {}
+
+	void DrawNumber(int num, float x, float y);
 };
