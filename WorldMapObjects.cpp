b660979ccb7381e4987c449cb9587cb9b19b7b93
Nguyễn Phạm Quốc Bảo <21521856@ms.uit.edu.vn>
2024-01-11 02:38:57 +0700

    Add the WorldMapObjects class, reuse the code from the IntroObjects class. Prepare for loading the mario world map.

---
+++ WorldMapObjects.cpp
@@ -0,0 +1,24 @@
+#include "WorldMapObjects.h"
+
+void CWorldMapObjects::Render()
+{
+	int aniId = -1;
+
+	switch (objectType)
+	{
+	case INTRO_BACKGROUND:
+		aniId = ID_ANI_INTRO_BACKGROUND;
+		break;
+	case NUMBER_3:
+		aniId = ID_ANI_NUMBER_3;
+		break;
+	case ARROW:
+		if (isOnePlayer)
+			aniId = ID_ANI_ARROW_1_PLAYER_GAME;
+		else
+			aniId = ID_ANI_ARROW_2_PLAYER_GAME;
+		break;
+	}
+
+	CAnimations::GetInstance()->Get(aniId)->Render(x, y);
+}
---
+++ WorldMapObjects.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "GameObject.h"
+#include "Animation.h"
+#include "Animations.h"
+#include "AssetIDs.h"
+
+#define	UNKNOWN				-1
+#define	INTRO_BACKGROUND	1
+#define	NUMBER_3			2
+#define	ARROW				3
+
+class CWorldMapObjects : public CGameObject
+{
+protected:
+	int objectType;
+	bool isOnePlayer;
+public:
+	CWorldMapObjects(float x, float y, int objectType) : CGameObject(x, y)
+	{
+		this->objectType = objectType;
+		isOnePlayer = true;
+	}
+
+	bool GetIsOnePlayer() { return isOnePlayer; }
+	void SetIsOnePlayer(bool b) { isOnePlayer = b; }
+
+	void Render();
+	void Update(DWORD dt) {}
+	void GetBoundingBox(float& l, float& t, float& r, float& b) {}
+};
