f6b1818a9e0625d5af41d03f3b5446b76804d56b
Nguyễn Phạm Quốc Bảo <21521856@ms.uit.edu.vn>
2024-01-13 16:37:05 +0700

    Reuse the code from class  Mushroom for the Effects and HammerBros (Separate the 2 objects in the WorldMapObjects class into 2 smaller classes for easier management)

---
+++ Effects.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "GameObject.h"
+#include "AssetIDs.h"
+#include "Pipe.h"
+#include "Mario.h"
+#include "Playscene.h"
+
+#define MUSHROOM_GRAVITY 0.001f
+#define MUSHROOM_WALKING_SPEED 0.05f
+
+#define MUSHROOM_RISEUP_SPEED 0.02f
+#define MUSHROOM_RISEUP_HEIGHT_MAX	16.0f
+
+#define MUSHROOM_BBOX_WIDTH 15
+#define MUSHROOM_BBOX_HEIGHT 15
+
+#define MUSHROOM_STATE_WAKEUP		100
+#define MUSHROOM_STATE_WALKING		200
+
+class CEffects : public CGameObject
+{
+protected:
+	float ax;
+	float ay;
+	float Origin_Y;
+
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+	void Render();
+	void GetBoundingBox(float& l, float& t, float& r, float& b);
+
+	int IsCollidable() { return 1; };
+	int IsBlocking() { return 0; }
+	void OnNoCollision(DWORD dt);
+	void OnCollisionWith(LPCOLLISIONEVENT e);
+
+public:
+	CEffects(float x, float y);
+	void SetState(int state);
+};
---
+++ HammerBros.cpp
@@ -0,0 +1,86 @@
+#include "HammerBros.h"
+
+CHammerBros::CHammerBros(float x, float y) :CGameObject(x, y)
+{
+	/*this->ax = 0;
+	this->ay = MUSHROOM_GRAVITY;
+	this->vx = 0;
+	this->vy = 0;
+	this->Origin_Y = y;
+	this->SetState(MUSHROOM_STATE_WAKEUP);*/
+}
+
+void CHammerBros::GetBoundingBox(float& l, float& t, float& r, float& b)
+{
+	/*l = x - MUSHROOM_BBOX_WIDTH / 2;
+	t = y - MUSHROOM_BBOX_HEIGHT / 2;
+	r = l + MUSHROOM_BBOX_WIDTH;
+	b = t + MUSHROOM_BBOX_HEIGHT;*/
+}
+
+void CHammerBros::OnNoCollision(DWORD dt)
+{
+	x += vx * dt;
+	y += vy * dt;
+}
+
+void CHammerBros::OnCollisionWith(LPCOLLISIONEVENT e)
+{
+	/*if (!e->obj->IsBlocking()) return;
+	if (dynamic_cast<CMushroom*>(e->obj)) return;
+	if (e->ny != 0)
+	{
+		vy = 0;
+	}
+	else if (e->nx != 0)
+	{
+		vx = -vx;
+	}*/
+}
+
+void CHammerBros::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	/*if (state == MUSHROOM_STATE_WALKING)
+	{
+		vx += ax * dt;
+		vy += ay * dt;
+	}
+	else if (state == MUSHROOM_STATE_WAKEUP)
+	{
+		if (Origin_Y - y < MUSHROOM_RISEUP_HEIGHT_MAX)
+		{
+			vy = -MUSHROOM_RISEUP_SPEED;
+			vx = 0;
+		}
+		else
+		{
+			SetState(MUSHROOM_STATE_WALKING);
+		}
+	}*/
+	CGameObject::Update(dt, coObjects);
+	CCollision::GetInstance()->Process(this, dt, coObjects);
+}
+
+void CHammerBros::Render()
+{
+	CAnimations::GetInstance()->Get(ID_ANI_MUSHROOM)->Render(x, y);
+}
+
+void CHammerBros::SetState(int state)
+{
+	/*CMario* mario = (CMario*)((LPPLAYSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
+	switch (state)
+	{
+	case MUSHROOM_STATE_WALKING:
+		if (this->x < mario->GetX())
+		{
+			vx = MUSHROOM_WALKING_SPEED;
+		}
+		else
+		{
+			vx = -MUSHROOM_WALKING_SPEED;
+		}
+		break;
+	}*/
+	CGameObject::SetState(state);
+}
