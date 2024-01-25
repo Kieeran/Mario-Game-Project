ef45380f72951316ef99d0f7dfd1036c48f637d9
Nguyễn Phạm Quốc Bảo <21521856@ms.uit.edu.vn>
2024-01-26 02:47:48 +0700

    Change the mechanism of the card from loading 1 animation containing 3 cards to loading 3 separate animations

--- Card.cpp
+++ Card.cpp
@@ -1,9 +1,42 @@
 #include "Card.h"
 
+void CCard::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	if (!isCollected)
+	{
+		if (GetTickCount64() - change_start > TIME_CHANGE_CARD && change_start > 0)
+		{
+			if (card == STAR_CARD)
+				card = MUSHROOM_CARD;
+			else if (card == MUSHROOM_CARD)
+				card = PLANT_CARD;
+			else if (card == PLANT_CARD)
+				card = STAR_CARD;
+			change_start = GetTickCount64();
+		}
+	}
+
+	CGameObject::Update(dt, coObjects);
+	CCollision::GetInstance()->Process(this, dt, coObjects);
+}
+
 void CCard::Render()
 {
 	CAnimations* animations = CAnimations::GetInstance();
-	animations->Get(ID_ANI_CARD)->Render(x, y);
+	int aniId = -1;
+	switch (card)
+	{
+	case STAR_CARD:
+		aniId = ID_ANI_STAR_CARD;
+		break;
+	case MUSHROOM_CARD:
+		aniId = ID_ANI_MUSHROOM_CARD;
+		break;
+	case PLANT_CARD:
+		aniId = ID_ANI_PLANT_CARD;
+		break;
+	}
+	animations->Get(aniId)->Render(x, y);
 	//RenderBoundingBox();
 }
 
--- Card.h
+++ Card.h
@@ -5,14 +5,38 @@
 #include "Animations.h"
 #include "AssetIDs.h"
 
+#define STAR_CARD		1
+#define MUSHROOM_CARD	2
+#define PLANT_CARD		3
+
+#define TIME_CHANGE_CARD 150
+
 #define CARD_BBOX_WIDTH 16
 #define CARD_BBOX_HEIGHT 16
 
 class CCard : public CGameObject
 {
+protected:
+	int card;
+	ULONGLONG change_start;
+	bool isCollected;
+
 public:
-	CCard(float x, float y) : CGameObject(x, y) {}
+	CCard(float x, float y) : CGameObject(x, y)
+	{
+		card = STAR_CARD;
+		change_start = GetTickCount64();
+		isCollected = false;
+	}
 	void Render();
-	void Update(DWORD dt) {}
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
+
+	void OnNoCollision(DWORD dt) {
+		x += vx * dt;
+		y += vy * dt;
+	}
+
+	virtual int IsCollidable() { return 1; }
+	virtual int IsBlocking() { return 1; }
 };
